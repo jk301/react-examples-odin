a7a407169d0badc9d3b59951bacaccb92fa3e62d
junaid <jk7948677@gmail.com>
2026-06-06 14:37:08 +0530

    did

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import './App.css';
 
-const COLORS = ['pink', 'green', 'blue', 'yellow', 'purple'];
+const COLORS = ['pink', 'green', 'blue', 'yellow', 'purple', 'magenta'];
 
 function App() {
-  const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+  const [backgroundColor, setBackgroundColor] = useState(COLORS[2]);
+  const [count, setCount] = useState(0)
 
   const onButtonClick = (color) => () => {
     setBackgroundColor(color);
+    setCount(count => count + 1)
   };
 
   return (
@@ -27,6 +29,10 @@ function App() {
           {color}
         </button>
       ))}
+      <p style={{
+        fontSize: "35px",
+        fontWeight: 900
+      }}>Background changes: {count}</p>
     </div>
   );
 }
